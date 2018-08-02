33a4c5869d0579e47861bf9c58b1b65bb247bc37
user-penguin <kobzev96@gmail.com>
2018-08-03 00:48:57 +0700

    fix html, now json - ok

--- ui/src/main/java/reserve/control/web/TodoController.java
+++ ui/src/main/java/reserve/control/web/TodoController.java
@@ -1,5 +1,6 @@
 package reserve.control.web;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import reserve.control.domain.*;
+
+import java.util.ArrayList;
 
 @Controller
 public class TodoController {
@@ -19,28 +22,20 @@ public class TodoController {
     public String login() {	
         return "login";
     }		
-	
+
 	// Show all todos
     @RequestMapping(value="/employees")
     public String todoList(Model model) {
-        JSONObject json = new JSONObject("{\"key\":\"mudak\", \"jey\":\"loh\"}");
-        model.addAttribute("employees", json);
+        JSONArray jArr = new JSONArray("[{\"first_name\":\"dmitry\", \"last_name\":\"kobzev\", \"age\":\"18\"}," +
+                "{\"first_name\":\"egor\", \"last_name\":\"pravdin\", \"age\":\"17\"}," +
+                "{\"first_name\":\"alex\", \"last_name\":\"demchenko\", \"age\":\"17\"}]");
+        ArrayList<JSONObject> jList = new ArrayList<>();
+        for(int i = 0; i < jArr.length(); i++)
+            jList.add(jArr.getJSONObject(i));
+        model.addAttribute("employeeList", jList);
         return "employees";
     }
-//
-//    // Add new todo
-//    @RequestMapping(value = "/addEmp")
-//    public String addEmp(Model model){
-//    	//model.addAttribute("emp", new Employee());
-//        return "addEmp";
-//    }
-//
-//    // Save new todo
-//    @RequestMapping(value = "/saveEmp", method = RequestMethod.POST)
-//    public String save(Employee employee){
-//        //employeeRepository.save(employee);
-//        return "redirect:employees";
-//    }
+
 
 
 }
