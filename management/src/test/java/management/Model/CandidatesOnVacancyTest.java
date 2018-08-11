d39627288d88161a52d935f8b8369ef3dd951c87
user-penguin <kobzev96@gmail.com>
2018-08-11 16:40:38 +0700

    create COV-test

--- management/src/test/java/management/Model/CandidatesOnVacancyTest.java
+++ management/src/test/java/management/Model/CandidatesOnVacancyTest.java
@@ -1,5 +1,24 @@
+package management.Model;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
 import static org.junit.Assert.*;
 
 public class CandidatesOnVacancyTest {
 
+    @Test
+    public void toStringJSONTest() {
+        CandidatesOnVacancy cov = new CandidatesOnVacancy();
+        cov.setVacancyId(12);
+        ArrayList<Integer> cand = new ArrayList<>();
+        cand.add(2);
+        cand.add(3);
+        cov.setCandidateIds(cand);
+
+        String expected = cov.toString();
+        String actual = "{\"vacId\":12,\"canNums\":[{\"canId\":2},{\"canId\":3}]}";
+        assertArrayEquals(expected.toCharArray(), actual.toCharArray());
+    }
 }
